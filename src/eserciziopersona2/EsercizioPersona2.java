3b1ab7211586d7eb26d82b1e51dc79ebe29e0cd5
LAB-INFORM-1-PC18 <LAB-INFORM-1-PC18@DESKTOP-VDDPTVH>
2019-10-15 10:47:49 +0200

    commit "inizio"

---
+++ src/eserciziopersona2/EsercizioPersona2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eserciziopersona2;
+
+/**
+ *
+ * @author LAB-INFORM-1-PC18
+ */
+public class EsercizioPersona2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
